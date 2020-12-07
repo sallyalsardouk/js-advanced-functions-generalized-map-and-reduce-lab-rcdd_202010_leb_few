f6005e326030a117eddc1d84d7afbf88a94877f0
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-07 11:10:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(src, fnct){
+    let arr = []
+
+    for (let i = 0; i < src.length; i++ ){
+        let elem = src[i]
+        arr.push(fnct(elem))
+    }
+    return arr;
+}
+
+function reduce(src, fnct, start){
+    let arr = (!!start) ? start : src[0];
+    let i = (!!start) ? 0 : 1 
+
+    for (; i < src.length; i++){
+        arr = fnct(src[i], arr)
+    }
+    return arr;
+}
