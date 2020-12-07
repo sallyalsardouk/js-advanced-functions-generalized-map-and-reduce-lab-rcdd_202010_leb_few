465e3126990a661bf0b630a632571b97c27cae71
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-07 11:10:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function reduce(src, fnct, start){
     let arr = (!!start) ? start : src[0];
     let i = (!!start) ? 0 : 1 
 
-    for (let i=0; i < src.length; i++){
+    for (; i < src.length; i++){
         arr = fnct(src[i], arr)
     }
     return arr;
