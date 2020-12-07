686bb2341e9c49aa7f55525e1489bed7850fdc73
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-07 10:59:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(src, callback){
+  let r=[];
+  for(let i=0;i<src.length;i++){
+    let e=src[i];
+    r.push(callback(e));
+  }
+  return r;
+}
+
+function reduce(src, cb, starting){
+  let r= (!!starting) ? starting : src[0];
+  let i= (!!starting) ? 0 : 1;
+  for(;i<src.length;i++){
+    r=cb(src[i],r);
+  }
+  return r;
+}
